9f8e5e5baaf13cd073af8530915d31bf70d8a5d0
LAPTOP-T4IEN2K1\TUF_Gaming <dongdinhvan.edu@gmail.com>
2023-07-17 12:17:24 +0700

    C# ListView Demo

---
+++ ListView-Demo/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace ListView_Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listViewStudent = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listViewStudent
+            // 
+            this.listViewStudent.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.listViewStudent.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listViewStudent.HideSelection = false;
+            this.listViewStudent.Location = new System.Drawing.Point(0, 272);
+            this.listViewStudent.Name = "listViewStudent";
+            this.listViewStudent.Size = new System.Drawing.Size(1193, 396);
+            this.listViewStudent.TabIndex = 0;
+            this.listViewStudent.UseCompatibleStateImageBehavior = false;
+            this.listViewStudent.SelectedIndexChanged += new System.EventHandler(this.listViewStudent_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(34, 189);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 50);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Delete";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(188, 189);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 50);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Insert\r\n";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(343, 189);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(131, 50);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(39, 97);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(435, 50);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "Find Student By ID";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1193, 668);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listViewStudent);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listViewStudent;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ ListView-Demo/Form1.cs
@@ -0,0 +1,131 @@
+﻿using ListView_Demo.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace ListView_Demo
+{
+    public partial class Form1 : Form
+    {
+
+       static List<Student> students = new List<Student>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            listViewStudent.View = View.Details;
+            // Allow the user to edit item text.
+            listViewStudent.LabelEdit = true;
+            // Allow the user to rearrange columns.
+          //  listViewStudent.AllowColumnReorder = true;
+            // Display check boxes.
+            listViewStudent.CheckBoxes = true;
+            // Select the item and subitems when selection is made.
+            listViewStudent.FullRowSelect = true;
+            // Display grid lines.
+            listViewStudent.GridLines = true;
+            // Sort the items in the list in ascending order.
+           // listViewStudent.Sorting = SortOrder.Ascending;
+            listViewStudent.Columns.Add("Index", -2, HorizontalAlignment.Left);
+            listViewStudent.Columns.Add("StudentCode", -2, HorizontalAlignment.Left);
+            listViewStudent.Columns.Add("StudentName", -2, HorizontalAlignment.Left);
+            listViewStudent.Columns.Add("Address", -2, HorizontalAlignment.Center);
+            listViewStudent.Columns.Add("Phone", -2, HorizontalAlignment.Center);
+            listViewStudent.Columns.Add("GPA", -2, HorizontalAlignment.Center);
+            listViewStudent.Columns.Add("Status", -2, HorizontalAlignment.Center);
+            listViewStudent.Columns.Add("Gender", -2, HorizontalAlignment.Center);
+            listViewStudent.Columns.Add("BirthDay", -2, HorizontalAlignment.Center);
+            listViewStudent.Columns.Add("Class", -2, HorizontalAlignment.Center);
+            listViewStudent.Columns.Add("Major", -2, HorizontalAlignment.Center);
+
+            // Khoi tao doi tuong List Student 
+
+            for(int i = 0; i < 10; i++)
+            {
+                // Them 10 Student vao studentList 
+                Student student = new Student();
+                student.Index = i+1;
+                student.StudentCode = "BH001";
+                student.StudentName = "Nguyen Tuan Manh";
+                student.Address = "Ha Noi";
+                student.Phone = "0945312678";
+                student.GPA = 8.0;
+                student.Status = 1;
+                student.Gender = true;
+                student.BirthDay = "2004-06-12";
+                student.Class = "SE06012";
+                student.Major = "Computer Science";
+
+                students.Add(student);
+
+            }
+
+            foreach (Student s in students)
+            {
+                listViewStudent.Items.Add(
+               new ListViewItem(new[] {
+                   s.Index+"", 
+                   s.StudentCode+"", 
+                   s.StudentName+"", 
+                   s.Address+"",
+                   s.Phone,s.GPA+"",
+                   s.Status+"",
+                   s.Gender+"",
+                   s.BirthDay+"",
+                   s.Class+"",
+                   s.Major }));
+            }
+
+           
+
+
+            // this.Controls.Add(listViewStudent);
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listViewStudent_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int index = listViewStudent.FocusedItem.Index;
+            students.RemoveAt(index);
+
+            listViewStudent.Items.Clear();
+
+            foreach (Student s in students)
+            {
+                listViewStudent.Items.Add(
+               new ListViewItem(new[] {
+                   s.Index+"",
+                   s.StudentCode+"",
+                   s.StudentName+"",
+                   s.Address+"",
+                   s.Phone,s.GPA+"",
+                   s.Status+"",
+                   s.Gender+"",
+                   s.BirthDay+"",
+                   s.Class+"",
+                   s.Major }));
+            }
+            listViewStudent.Refresh();
+            
+           // MessageBox.Show(index + "");
+
+
+            // int index = listViewStudent.SelectedItems.IndexOf(selectedItems);
+
+        }
+    }
+}
